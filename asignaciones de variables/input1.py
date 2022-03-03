fff84ccace4ea15fc75a0c6ad0fb1f2d015496f4
JosueMd <82351031+itzJosueC@users.noreply.github.com>
2022-03-03 07:31:05 -0500

    Delete asignaciones de variables directory

--- asignaciones de variables/input1.py
+++
@@ -1,7 +0,0 @@
-nombre = input ("Ingrese su nombre:")
-apellido = input ("Ingrese su apellido:")
-edad = int(input ("Ingrese su edad:"))
-
-print("Su nombre es:", nombre)
-print("Su apellido es:", apellido)
-print("Su edad es:", edad + 1)
